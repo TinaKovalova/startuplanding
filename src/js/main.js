c1faa7b16c47e94e88a433bf369b2e86fff76bcd
TinaKovalova <kovalevatkv@gmail.com>
2025-09-08 23:02:07 +0300

    header

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,11 @@ import "/src/scss/style.scss";
 
 window.addEventListener("load", () => {
 
+    const header = document.querySelector(".header");
+    const burgerButton = document.querySelector(".burger-button");
+    burgerButton?.addEventListener("click", () => {
+        header?.classList.toggle("_active-menu");
+    });
  
 
 });
