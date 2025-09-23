17bd552337f4bafa40963fd10304d5ebc37ef243
TinaKovalova <kovalevatkv@gmail.com>
2025-09-23 20:52:26 +0300

    swiper slide image, menu-burger color,scroller

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,9 @@ import { defineConfig } from "vite";
 import injectHTML from "vite-plugin-html-inject";
 import VitePluginWebpAndPath from "vite-plugin-webp-and-path";
 
-
-// const __dirname = dirname(fileURLToPath(import.meta.url));
-
 export default defineConfig({
-  // base:"//",
-  plugins: [
-    injectHTML(), VitePluginWebpAndPath()],
+  base: "/startuplanding/",
+  plugins: [injectHTML(), VitePluginWebpAndPath()],
 });
 
 
